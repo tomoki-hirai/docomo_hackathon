1af35d85b44024e9f7139c2caab76c7a00ef190d
tomoki-hirai <thirai@mikilab.doshisha.c.jp>
2018-09-21 02:37:03 +0900

    エンドポイント作成，chat使えるか確認

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/mrtry', function(req, res, next) {
+  res.render('mrtry', { title: 'Express' });
+});
 router.get('/tag', function(req, res, next) {
   res.render('tag', { title: 'Express' });
 });
+router.get('/tabu', function(req, res, next) {
+  res.render('tabu', { title: 'Express' });
+});
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' });
+});
+
+
 
 module.exports = router;
